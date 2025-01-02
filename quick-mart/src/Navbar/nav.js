c708e4ee1f27a621c6a758350b986a0ec66683e2
jineesh09 <jdesai1495@gmail.com>
2025-01-02 11:21:18 +0530

    updated

--- quick-mart/src/Navbar/nav.js
+++ quick-mart/src/Navbar/nav.js
@@ -14,17 +14,7 @@ function Navbar(){
               <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"/></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
+              
               <li class="nav-item">
                 <a class="nav-link disabled" aria-disabled="true">Disabled</a>
               </li>
@@ -33,6 +23,20 @@ function Navbar(){
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
               <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
+            <ul className="navbar-nav">
+            <li className="nav-item">
+              {/* <Link className="nav-link text-dark mx-3 " to="/home/cart"><i class="fa-solid fa-cart-shopping" style={{fontSize: "20px"}}></i>Cart</Link> */}
+            </li>
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Admin
+                </a>
+                <ul className="dropdown-menu">
+                  <li><hr className="dropdown-divider"/></li>
+                  <li>Log Out</li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
